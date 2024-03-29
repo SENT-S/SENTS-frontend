b5bf2715aca6deb3a2d1207a692a3e3061f59fb9
Ochieng Paul <ochiengpaul442@gmail.com>
2024-03-29 22:56:39 +0300

    new website template

--- Website/vite.config.ts
+++ Website/vite.config.ts
@@ -1,33 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  root: "app",
-  // base: "/static/",
-  build: {
-    manifest: true,
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: "app/src/main.tsx",
-      },
-      output: {
-        chunkFileNames: "chunks/[name].js",
-        assetFileNames: "assets/[name].[ext]",
-        entryFileNames: "assets/[name].js",
-      },
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
