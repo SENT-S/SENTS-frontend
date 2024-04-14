e5aada63e29fb28751b64d31d1eabe6d8bb90a4d
Ochieng Paul <ochiengpaul442@gmail.com>
2024-04-14 23:50:43 +0300

    Update website files and remove unused code

--- website/src/services/urls/index.tsx
+++
@@ -1,8 +0,0 @@
-const COUNTRY_URL = import.meta.env.VITE_COUNTRY_FLAG_URL;
-const FINANCIALS_URL = import.meta.env.VITE_FINANCIALS_URL;
-
-export const GET_COUNTRY_FLAG = `${COUNTRY_URL}/flag`;
-
-export const GET_FINANCIAL = `${FINANCIALS_URL}/financials`;
-
-export const GET_COMPANY_DATA = `${FINANCIALS_URL}/companies`;
