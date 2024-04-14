7eeceb674db9281131ba30a16895841e90ecc647
Ochieng Paul <ochiengpaul442@gmail.com>
2024-04-14 14:23:23 +0300

    Update ThemeProvider to enable light theme by default

--- sents-web/src/app/layout.tsx
+++ sents-web/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         <Provider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
--- sents-web/src/components/themeProvider/ThemeProvider.tsx
+++ sents-web/src/components/themeProvider/ThemeProvider.tsx
@@ -1,9 +1,17 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { ThemeProvider as NextThemeProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
+import type { ThemeProviderProps } from 'next-themes/dist/types';
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
 };
 
