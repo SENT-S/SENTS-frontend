d7903ee39e75e4315c77d9bc2396960591913137
Ochieng Paul <ochiengpaul442@gmail.com>
2024-07-14 17:05:28 +0300

    refactor: Update Breadcrumb component to include breadcrumb navigation in Add Company page

--- sents-web/src/app/(admin_pages)/new_company/page.tsx
+++ sents-web/src/app/(admin_pages)/new_company/page.tsx
@@ -5,49 +5,69 @@ import Stepper from '@/components/ui/Stepper';
 import Step_1 from './_steps/step_1';
 import Step_2 from './_steps/step_2';
 import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import {
   getAllFinancialDataCategories,
   getAllFinancialMetrics,
 } from '@/services/apis/companies';
 
-const page = () => {
+const steps = ['Step 1', 'Step 2'];
+
+const fetchFinancialData = async () => {
+  const [categoriesRes, metricsRes] = await Promise.all([
+    getAllFinancialDataCategories(),
+    getAllFinancialMetrics(),
+  ]);
+
+  return { categories: categoriesRes, metrics: metricsRes.data };
+};
+
+const AddCompanyPage = () => {
   const [step, setStep] = useState(1);
-  const steps = ['Step 1', 'Step 2'];
   const [category, setCategory] = useState([]);
   const [metrics, setMetrics] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const [categoriesRes, metricsRes] = await Promise.all([
-        getAllFinancialDataCategories(),
-        getAllFinancialMetrics(),
-      ]);
-
-      setCategory(categoriesRes);
-      setMetrics(metricsRes.data);
-    };
-
-    fetchData();
+    fetchFinancialData().then(({ categories, metrics }) => {
+      setCategory(categories);
+      setMetrics(metrics);
+    });
   }, []);
 
   return (
     <MainLayout>
-      <div className="space-y-8 text-center">
+      <div className="space-y-8 mt-4 text-center">
+        <Breadcrumb className="bg-white rounded-md shadow-md p-4 dark:bg-[#39463E80]">
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/dashboard" className="text-green-600">
+                Dashboard
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Add new Company</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+
         <h1>Add new Company</h1>
         <Stepper currentStep={step} steps={steps} />
 
         {/* Display steps */}
         {step === 1 && <Step_1 setStep={setStep} step={step} />}
         {step === 2 && (
-          <Step_2
-            setStep={setStep}
-            step={step}
-            category={category}
-            metrics={metrics}
-          />
+          <Step_2 setStep={setStep} category={category} metrics={metrics} />
         )}
       </div>
     </MainLayout>
   );
 };
 
-export default page;
+export default AddCompanyPage;
