7c4de61db272c60f3830d23a26d7e34afcddc490
Ochieng Paul <ochiengpaul442@gmail.com>
2024-07-18 04:09:57 +0300

    refactor: Update StoreProvider component to handle session token expiration

--- sents-web/src/app/StoreProvider.tsx
+++ sents-web/src/app/StoreProvider.tsx
@@ -1,11 +1,11 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
-import { store } from '@/lib/store';
+import { store } from '../lib/store';
 import { useSession, signOut } from 'next-auth/react';
 import jwt from 'jsonwebtoken';
 import { CustomSession } from '@/utils/types';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 interface ProviderProps {
   children: React.ReactNode;
@@ -17,39 +17,30 @@ interface DecodedToken {
 
 const StoreProvider = ({ children }: ProviderProps) => {
   const { data: session } = useSession() as { data: CustomSession };
-  const router = useRouter();
-  const [redirecting, setRedirecting] = useState(false);
 
   useEffect(() => {
-    if (!session || !session.token) {
-      if (!redirecting) {
-        setRedirecting(true);
-        signOut();
-        localStorage.clear();
-        router.push('/login_register');
+    const isTokenExpiredOrSessionUndefined = () => {
+      if (!session || !session.token) {
+        return true;
       }
-      return;
-    }
+      try {
+        const decoded = jwt.decode(session.token) as DecodedToken;
+        if (!decoded || !decoded.exp) {
+          return true;
+        }
+        const currentTime = Date.now() / 1000;
+        return decoded.exp < currentTime;
+      } catch (error) {
+        return true;
+      }
+    };
 
-    try {
-      const decoded = jwt.decode(session.token) as DecodedToken;
-      if (!decoded || !decoded.exp) {
-        throw new Error('Invalid token');
-      }
-
-      const currentTime = Date.now() / 1000;
-      if (decoded.exp < currentTime) {
-        throw new Error('Token expired');
-      }
-    } catch (error) {
-      if (!redirecting) {
-        setRedirecting(true);
-        signOut();
-        localStorage.clear();
-        router.push('/login_register');
-      }
+    if (isTokenExpiredOrSessionUndefined() && !session) {
+      signOut();
+      localStorage.clear();
+      redirect('/login_register');
     }
-  }, []);
+  }, [session]);
 
   return <Provider store={store}>{children}</Provider>;
 };
