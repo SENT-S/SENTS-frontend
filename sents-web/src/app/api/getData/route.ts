50043d1958bb7f376b6b2d47b22c011e654ad62c
Ochieng Paul <ochiengpaul442@gmail.com>
2024-08-17 22:47:54 +0300

    refactor: Update getData route to return JSON responses
    This commit updates the getData route in the sents-web/src/app/api/getData/route.ts file to return JSON responses instead of plain text. The response objects are now properly formatted with the appropriate content type header. This refactor improves the consistency and compatibility of the API responses.

--- sents-web/src/app/api/getData/route.ts
+++ sents-web/src/app/api/getData/route.ts
@@ -14,37 +14,47 @@ config({ path: path.resolve(process.cwd(), '.env.local') });
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
-  const session = await getServerSession(authOptions);
-  const endpoint = req.nextUrl.searchParams.get('endpoint');
-  const authorization = req.headers.get('Authorization');
-
-  if (!session && !authorization) {
-    return NextResponse.json({ data: 'Unauthorized user connection' }, { status: 401 });
-  }
-
-  if (!endpoint) {
-    return NextResponse.json({ data: 'Endpoint not specified' }, { status: 400 });
-  }
-
-  const token = session ? (session as CustomSession).token : null;
-  const url = `${BASE_URL}/${endpoint}`;
-
   try {
+    const session = await getServerSession(authOptions);
+    const endpoint = req.nextUrl.searchParams.get('endpoint');
+    const authorization = req.headers.get('Authorization');
+
+    if (!session && !authorization) {
+      return new NextResponse(JSON.stringify({ data: 'Unauthorized user connection' }), {
+        status: 401,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+
+    if (!endpoint) {
+      return new NextResponse(JSON.stringify({ data: 'Endpoint not specified' }), {
+        status: 400,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+
+    const token = session ? (session as CustomSession).token : null;
+    const url = `${BASE_URL}/${endpoint}`;
+
     const headers = {
       Authorization: authorization || `Bearer ${token}`,
     };
 
     const response = await axios.get(url, { headers });
-
     const { data } = response.data;
 
-    return NextResponse.json({ data }, { status: 200 });
+    return new NextResponse(JSON.stringify({ data }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
   } catch (error: any) {
     console.error('Error fetching data:', error);
-    return NextResponse.json(
-      { data: 'Error fetching data', error: error.message },
-      { status: 500 },
-    );
+    return new NextResponse(JSON.stringify({ data: 'Error fetching data', error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
   }
 }
